6d3e7d75ad3ef43c78b540ca8ee0320c6d85d8b4
wumbles <77227394+wumbles@users.noreply.github.com>
2023-03-08 00:37:52 -0700

    UPDATE: working on Create Property form

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+import {
+  Box,
+  Typography,
+  FormControl,
+  FormHelperText,
+  TextField,
+  TextareaAutosize,
+  Stack,
+  Select,
+  MenuItem,
+  Button,
+} from "@pankod/refine-mui";
 
-const Form = () => {
-  return <div>Form</div>;
+import { FormProps } from "interfaces/common";
+import CustomButton from "./CustomButton";
+
+const Form = ({
+  type,
+  register,
+  handleSubmit,
+  handleImageChange,
+  formLoading,
+  onFinishHandler,
+  propertyImage,
+}: FormProps) => {
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={600} color="#11142d">
+        {type} a Property
+      </Typography>
+
+      <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#fcfcfc">
+        <form
+          style={{
+            marginTop: "20px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+          onSubmit={handleSubmit(onFinishHandler)}
+        >
+          <FormControl>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px",
+                fontSize: 16,
+                color: "#11142d",
+              }}
+            >
+              Enter property name
+            </FormHelperText>
+            <TextField
+              fullWidth
+              required
+              id="outlined-basic"
+              color="info"
+              variant="outlined"
+              {...register("title", { required: true })}
+            />
+          </FormControl>
+          <FormControl>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px",
+                fontSize: 16,
+                color: "#11142d",
+              }}
+            >
+              Enter Description
+            </FormHelperText>
+            <TextareaAutosize
+              minRows={5}
+              required
+              placeholder="Write description"
+              color="info"
+              style={{
+                width: "100%",
+                background: "transparent",
+                fontSize: "16px",
+                borderColor: "rgba(0,0,0,0.23)",
+              }}
+            />
+          </FormControl>
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
 export default Form;
