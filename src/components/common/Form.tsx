637050edd21202577a7ebbb8133fb44764937e43
wumbles <77227394+wumbles@users.noreply.github.com>
2023-03-10 18:30:28 -0700

    MILESTONE: wrapping up the property form

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,3 +1,4 @@
+import { TypeSpecimenOutlined } from "@mui/icons-material";
 import {
   Box,
   Typography,
@@ -9,6 +10,7 @@ import {
   Select,
   MenuItem,
   Button,
+  textTransform,
 } from "@pankod/refine-mui";
 
 import { FormProps } from "interfaces/common";
@@ -180,6 +182,55 @@ const Form = ({
               sx={{ input: { color: "#11142d" } }}
             />
           </FormControl>
+
+          <Stack direction="column" gap={1} justifyContent="center" mb={2}>
+            <Stack direction="row" gap={2}>
+              <Typography
+                color="#11142d"
+                fontSize={16}
+                fontWeight={500}
+                my="10px"
+              >
+                Property Photo
+              </Typography>
+
+              <Button
+                component="label"
+                sx={{
+                  width: "fit-content",
+                  color: "#2ed480",
+                  textTransform: "capitalize",
+                  fontSize: 16,
+                }}
+              >
+                Upload *
+                <input
+                  hidden
+                  accept="image/*"
+                  type="file"
+                  onChange={(e) => {
+                    // @ts-ignore
+                    handleImageChange(e.target.files[0]);
+                  }}
+                />
+              </Button>
+            </Stack>
+
+            <Typography
+              fontSize={14}
+              color="#808191"
+              sx={{ wordBreak: "break-all" }}
+            >
+              {propertyImage?.name}
+            </Typography>
+          </Stack>
+
+          <CustomButton
+            type="submit"
+            title={formLoading ? "Submitting..." : "Submit"}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+          />
         </form>
       </Box>
     </Box>
