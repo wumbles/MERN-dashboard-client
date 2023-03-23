415158abd8b710367dc97d926191ed2bbe318e1c
wumbles <77227394+wumbles@users.noreply.github.com>
2023-03-22 22:02:09 -0600

    UPDATE: Added pagination for properties + input styling

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,6 +1,13 @@
 import { Add } from "@mui/icons-material";
 import { useTable } from "@pankod/refine-core";
-import { Box, Stack, Typography } from "@pankod/refine-mui";
+import {
+  Box,
+  Stack,
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+} from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
 
 import { PropertyCard, CustomButton } from "components";
@@ -10,19 +17,85 @@ const AllProperties = () => {
 
   const {
     tableQueryResult: { data, isLoading, isError },
+    current,
+    setCurrent,
+    setPageSize,
+    pageCount,
+    sorter,
+    setSorter,
+    filters,
+    setFilters,
   } = useTable();
 
-  const AllProperties = data?.data ?? [];
+  const allProperties = data?.data ?? [];
 
   if (isLoading) return <Typography>Loading...</Typography>;
   if (isError) return <Typography>Error...</Typography>;
 
   return (
     <Box>
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        <Stack direction="column" width="100%">
+          <Typography fontSize={25} fontWeight={700} color="#11142d">
+            {!allProperties.length
+              ? "There are no properties"
+              : "All Properties"}
+          </Typography>
+          <Box
+            mb={2}
+            mt={3}
+            display="flex"
+            width="84%"
+            justifyContent="space-between"
+            flexWrap="wrap"
+          >
+            <Box
+              display="flex"
+              gap={2}
+              flexWrap="wrap"
+              mb={{ xs: "20px", sm: 0 }}
+            >
+              <CustomButton
+                title={`Sort Price`}
+                handleClick={() => {}}
+                backgroundColor="#475be8"
+                color="#fcfcfc"
+              />
+
+              <TextField
+                variant="outlined"
+                color="info"
+                placeholder="Search by title"
+                value=""
+                onChange={() => {}}
+                style={{
+                  border: "2px solid #475be8",
+                  borderRadius: "6px",
+                }}
+                sx={{ input: { color: "#000" } }}
+              />
+              <Select
+                variant="standard"
+                color="info"
+                required
+                displayEmpty
+                inputProps={{
+                  "aria-label": "Without label",
+                }}
+                defaultValue=""
+                value=""
+                onChange={() => {}}
+                sx={{ color: "#808191", padding: "10px" }}
+                style={{ border: "2px solid #475be8", borderRadius: "6px" }}
+              >
+                <MenuItem value="">All</MenuItem>
+              </Select>
+            </Box>
+          </Box>
+        </Stack>
+      </Box>
+
       <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Typography fontSize={25} fontWeight={600} color="#11142d">
-          All Properties
-        </Typography>
         <CustomButton
           title="Add Property"
           handleClick={() => navigate("/properties/create")}
@@ -33,7 +106,7 @@ const AllProperties = () => {
       </Stack>
 
       <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
-        {AllProperties.map((property) => (
+        {allProperties.map((property) => (
           <PropertyCard
             key={property._id}
             id={property._id}
@@ -44,6 +117,53 @@ const AllProperties = () => {
           />
         ))}
       </Box>
+
+      {allProperties.length > 0 && (
+        <Box display="flex" gap={2} mt={3} flexWrap="wrap">
+          <CustomButton
+            title="Previous"
+            handleClick={() => setCurrent((prev) => prev - 1)}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            disabled={!(current > 1)}
+          />
+          <Box
+            display={{ xs: "hidden", sm: "flex" }}
+            alignItems="center"
+            gap="5px"
+            color="#11142d"
+            fontWeight={500}
+          >
+            Page{" "}
+            <Typography fontWeight={500}>
+              {current} of {pageCount}
+            </Typography>
+          </Box>
+          <CustomButton
+            title="Next"
+            handleClick={() => setCurrent((prev) => prev + 1)}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            disabled={current === pageCount}
+          />
+          <Select
+            variant="standard"
+            color="info"
+            displayEmpty
+            required
+            inputProps={{
+              "aria-label": "Without label",
+            }}
+            defaultValue=""
+            value=""
+            onChange={() => {}}
+            sx={{ color: "#808191", padding: "10px" }}
+            style={{ border: "2px solid #475be8", borderRadius: "6px" }}
+          >
+            <MenuItem value="">All</MenuItem>
+          </Select>
+        </Box>
+      )}
     </Box>
   );
 };
