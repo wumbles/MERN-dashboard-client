018f9985c30545f9642a4c7be143592e2bacd2be
wumbles <77227394+wumbles@users.noreply.github.com>
2023-04-17 01:07:42 -0600

    MILESTONE: practice project should be now ready to be deployed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,31 @@
 import { useList } from "@pankod/refine-core";
-import { Typography, Box, Stack } from "@pankod/refine-mui";
+import { Typography, Box, Stack } from "@mui/material";
 
 import {
   PieChart,
   PropertyReferrals,
   TotalRevenue,
   PropertyCard,
-  TopAgent,
 } from "components";
 
-const home = () => {
+const Home = () => {
+  const { data, isLoading, isError } = useList({
+    resource: "properties",
+    config: {
+      pagination: {
+        pageSize: 4,
+      },
+    },
+  });
+
+  const latestProperties = data?.data ?? [];
+
+  if (isLoading) return <Typography>Loading...</Typography>;
+  if (isError) return <Typography>Something went wrong!</Typography>;
+
   return (
     <Box>
-      <Typography fontSize={25} fontWeight={600} color="#11142D">
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
         Dashboard
       </Typography>
 
@@ -20,26 +33,26 @@ const home = () => {
         <PieChart
           title="Properties for Sale"
           value={684}
-          series={[65, 35]}
-          colors={["#475ae8", "#e88547"]}
+          series={[75, 25]}
+          colors={["#275be8", "#c4e8ef"]}
         />
         <PieChart
           title="Properties for Rent"
           value={550}
-          series={[45, 55]}
-          colors={["#475ae8", "#e88547"]}
+          series={[60, 40]}
+          colors={["#275be8", "#c4e8ef"]}
         />
         <PieChart
-          title="Total Customers"
+          title="Total customers"
           value={5684}
-          series={[10, 90]}
-          colors={["#475ae8", "#e88547"]}
+          series={[75, 25]}
+          colors={["#275be8", "#c4e8ef"]}
         />
         <PieChart
-          title="Total Cities"
+          title="Properties for Cities"
           value={555}
-          series={[70, 30]}
-          colors={["#475ae8", "#e88547"]}
+          series={[75, 25]}
+          colors={["#275be8", "#c4e8ef"]}
         />
       </Box>
 
@@ -52,8 +65,36 @@ const home = () => {
         <TotalRevenue />
         <PropertyReferrals />
       </Stack>
+
+      <Box
+        flex={1}
+        borderRadius="15px"
+        padding="20px"
+        bgcolor="#fcfcfc"
+        display="flex"
+        flexDirection="column"
+        minWidth="100%"
+        mt="25px"
+      >
+        <Typography fontSize="18px" fontWeight={600} color="#11142d">
+          Latest Properties
+        </Typography>
+
+        <Box mt={2.5} sx={{ display: "flex", flexWrap: "wrap", gap: 4 }}>
+          {latestProperties.map((property) => (
+            <PropertyCard
+              key={property._id}
+              id={property._id}
+              title={property.title}
+              location={property.location}
+              price={property.price}
+              photo={property.photo}
+            />
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
 
-export default home;
+export default Home;
